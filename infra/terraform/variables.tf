760555f8e2b7f4308076553d9075484143167b56
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-24 13:10:16 -0600

    Refactor Terraform variables for clarity
    Removed unnecessary comments and streamlined variable definitions.

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,127 +1,30 @@
-###############################
-# General
-###############################
-
 variable "aws_region" {
-  type        = string
-  description = "AWS region"
+  type = string
 }
 
 variable "project_name" {
-  type        = string
-  description = "Project name used for naming resources"
-  default     = "kafka-enterprise-orders"
-}
-
-###############################
-# Docker Images from CI/CD
-###############################
-
-variable "container_image_producer" {
-  type        = string
-  description = "Docker image for order producer"
-}
-
-variable "container_image_fraud" {
-  type        = string
-  description = "Docker image for fraud service"
-}
-
-variable "container_image_payment" {
-  type        = string
-  description = "Docker image for payment service"
-}
-
-variable "container_image_analytics" {
-  type        = string
-  description = "Docker image for analytics service"
-}
-
-###############################
-# Confluent Cloud
-###############################
-
-variable "confluent_bootstrap_servers" {
-  type        = string
-  description = "Bootstrap servers for Confluent Cloud Kafka"
-}
-
-variable "confluent_api_key" {
-  type        = string
-  description = "Confluent Cloud API Key"
-}
-
-variable "confluent_api_secret" {
-  type        = string
-  description = "Confluent Cloud API Secret"
-}
-
-###############################
-# Kafka Topics
-###############################
-
-variable "orders_topic" {
   type    = string
-  default = "orders"
+  default = "kafka-enterprise-orders"
 }
 
-variable "fraud_alerts_topic" {
-  type    = string
-  default = "fraud-alerts"
-}
+variable "container_image_producer" { type = string }
+variable "container_image_fraud"    { type = string }
+variable "container_image_payment"  { type = string }
+variable "container_image_analytics" { type = string }
 
-variable "payments_topic" {
-  type    = string
-  default = "payments"
-}
+variable "confluent_bootstrap_servers" { type = string }
+variable "confluent_api_key" { type = string }
+variable "confluent_api_secret" { type = string }
 
-variable "order_analytics_topic" {
-  type    = string
-  default = "order-analytics"
-}
+variable "orders_topic" { type = string default = "orders" }
+variable "fraud_alerts_topic" { type = string default = "fraud-alerts" }
+variable "payments_topic" { type = string default = "payments" }
+variable "order_analytics_topic" { type = string default = "order-analytics" }
 
-###############################
-# Couchbase (only analytics service)
-###############################
-
-variable "couchbase_host" {
-  type    = string
-  default = "couchbase"
-}
-
-variable "couchbase_bucket" {
-  type    = string
-  default = "order_analytics"
-}
-
-variable "couchbase_username" {
-  type    = string
-  default = "Administrator"
-}
-
-variable "couchbase_password" {
-  type    = string
-  default = "password"
-}
-
-###############################
-# Networking (from Terraform)
-###############################
-
-variable "public_subnet_a" {
-  type = string
-}
-
-variable "public_subnet_b" {
-  type = string
-}
-
-# NOTE: ecs_tasks_sg REMOVED
-# Because we use aws_security_group.ecs_tasks.id directly
-
-###############################
-# RDS
-###############################
+variable "couchbase_host" { type = string default = "couchbase" }
+variable "couchbase_bucket" { type = string default = "order_analytics" }
+variable "couchbase_username" { type = string default = "Administrator" }
+variable "couchbase_password" { type = string default = "password" }
 
 variable "rds_password" {
   type      = string
