33f73a5d1638e2206d8dd3ad29ec137e9800c1e0
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-25 12:57:51 -0600

    Update ecs.tf

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -1,3 +1,7 @@
+########################################
+# ECS CLUSTER
+########################################
+
 resource "aws_ecs_cluster" "app_cluster" {
   name = "${var.project_name}-cluster"
 
@@ -8,44 +12,51 @@ resource "aws_ecs_cluster" "app_cluster" {
 }
 
 ########################################
-# LOG GROUPS
+# CLOUDWATCH LOG GROUPS
 ########################################
 
 resource "aws_cloudwatch_log_group" "producer" {
-  name = "/ecs/${var.project_name}-producer"
+  name              = "/ecs/${var.project_name}-producer"
   retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "fraud" {
-  name = "/ecs/${var.project_name}-fraud"
+  name              = "/ecs/${var.project_name}-fraud"
   retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "payment" {
-  name = "/ecs/${var.project_name}-payment"
+  name              = "/ecs/${var.project_name}-payment"
   retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "analytics" {
-  name = "/ecs/${var.project_name}-analytics"
+  name              = "/ecs/${var.project_name}-analytics"
   retention_in_days = 7
 }
 
 ########################################
-# IAM ROLES
+# IAM ROLES (IDEMPOTENT FOR CI/CD)
 ########################################
 
 resource "aws_iam_role" "ecs_task_execution" {
   name = "${var.project_name}-ecs-task-execution"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
       Action    = "sts:AssumeRole"
     }]
   })
+
+  lifecycle {
+    ignore_changes = [
+      name,
+      assume_role_policy
+    ]
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
@@ -57,17 +68,30 @@ resource "aws_iam_role" "ecs_task_role" {
   name = "${var.project_name}-ecs-task-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
       Action    = "sts:AssumeRole"
     }]
   })
+
+  lifecycle {
+    ignore_changes = [
+      name,
+      assume_role_policy
+    ]
+  }
+}
+
+# Add required permissions for your microservices
+resource "aws_iam_role_policy_attachment" "ecs_task_extra" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
 ########################################
-# TASK DEFS
+# TASK DEFINITION
 ########################################
 
 resource "aws_ecs_task_definition" "order_producer" {
@@ -79,28 +103,36 @@ resource "aws_ecs_task_definition" "order_producer" {
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name  = "order-producer"
-      image = var.container_image_producer
+  container_definitions = jsonencode([{
+    name      = "order-producer"
+    image     = var.container_image_producer
+    essential = true
 
-      environment = [
-        { name = "ORDERS_TOPIC", value = var.orders_topic },
-        { name = "KAFKA_BOOTSTRAP", value = var.confluent_bootstrap_servers },
-        { name = "KAFKA_API_KEY", value = var.confluent_api_key },
-        { name = "KAFKA_API_SECRET", value = var.confluent_api_secret }
-      ]
+    portMappings = [{
+      containerPort = 8080
+      protocol      = "tcp"
+    }]
 
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.producer.name
-          awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "producer"
-        }
+    environment = [
+      { name = "ORDERS_TOPIC", value = var.orders_topic },
+      { name = "KAFKA_BOOTSTRAP", value = var.confluent_bootstrap_servers },
+      { name = "KAFKA_API_KEY", value = var.confluent_api_key },
+      { name = "KAFKA_API_SECRET", value = var.confluent_api_secret }
+    ]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.producer.name
+        awslogs-region        = var.aws_region
+        awslogs-stream-prefix = "producer"
       }
     }
-  ])
+  }])
+
+  depends_on = [
+    aws_cloudwatch_log_group.producer
+  ]
 }
 
 ########################################
@@ -115,11 +147,12 @@ resource "aws_ecs_service" "order_producer" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = [
-      aws_subnet.public_a.id,
-      aws_subnet.public_b.id
-    ]
+    subnets         = var.private_subnets
     security_groups = [aws_security_group.ecs_tasks.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
+
+  depends_on = [
+    aws_ecs_task_definition.order_producer
+  ]
 }
