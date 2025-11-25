b2203780a85c73ca3580675b1485bb031265744e
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-25 13:04:28 -0600

    Refactor VPC configuration with public and private subnets

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "main" {
 }
 
 ########################################
-# Internet Gateway
+# INTERNET GATEWAY (for public subnets)
 ########################################
 
 resource "aws_internet_gateway" "igw" {
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 ########################################
-# Subnet A
+# PUBLIC SUBNETS
 ########################################
 
 resource "aws_subnet" "public_a" {
@@ -29,23 +29,71 @@ resource "aws_subnet" "public_a" {
   cidr_block              = "10.10.1.0/24"
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
-}
 
-########################################
-# Subnet B
-########################################
+  tags = {
+    Name = "${var.project_name}-public-a"
+  }
+}
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.10.2.0/24"
   availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-b"
+  }
 }
 
 ########################################
-# Route Table + Associations
+# PRIVATE SUBNETS (ECS + RDS)
 ########################################
 
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.11.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "${var.project_name}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.12.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name = "${var.project_name}-private-b"
+  }
+}
+
+########################################
+# NAT GATEWAY + Elastic IP
+########################################
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+}
+
+########################################
+# ROUTE TABLES
+########################################
+
+# Public route table → IGW
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -55,12 +103,32 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "public_a" {
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_a.id
 }
 
-resource "aws_route_table_association" "b" {
+resource "aws_route_table_association" "public_b" {
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_b.id
+}
+
+# Private route table → NAT gateway
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_a.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_b.id
 }
