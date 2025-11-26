4f5e24281b08d5a520215bf8f976b4a29f7fb794
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-25 18:49:32 -0600

    Update WAF configuration and rules in waf.tf

--- infra/terraform/waf.tf
+++ infra/terraform/waf.tf
@@ -1,31 +1,65 @@
 resource "aws_wafv2_web_acl" "webapp" {
-  name  = "${var.project_name}-waf"
-  scope = "REGIONAL"
+  name        = "webapp-waf"
+  description = "WAF for webapp"
+  scope       = "REGIONAL"
 
-  default_action { allow {} }
+  default_action {
+    allow {}
+  }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "waf"
+    metric_name                = "webapp-waf"
     sampled_requests_enabled   = true
   }
 
   rule {
-    name     = "AWS-Common"
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
     priority = 1
-    override_action { none {} }
+
+    override_action {
+      none {}
+    }
 
     statement {
       managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
         vendor_name = "AWS"
+        name        = "AWSManagedRulesCommonRuleSet"
       }
     }
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "common"
+      metric_name                = "common-rule-set"
       sampled_requests_enabled   = true
     }
   }
+
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "bad-inputs"
+      sampled_requests_enabled   = true
+    }
+  }
+}
+
+# Associate WAF with ALB
+resource "aws_wafv2_web_acl_association" "webapp" {
+  resource_arn = aws_lb.webapp_load_balancer.arn
+  web_acl_arn  = aws_wafv2_web_acl.webapp.arn
 }
