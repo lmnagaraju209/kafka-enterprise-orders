99ed67c05847605bf170fdb7cd7484a106388632
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 14:48:31 -0600

    Refactor security group data references in Terraform

--- infra/terraform/security_groups.tf
+++ infra/terraform/security_groups.tf
@@ -1,18 +1,15 @@
-##############################################
-# USE EXISTING SECURITY GROUPS (NO CREATION)
-##############################################
+############################################
+# Reuse Existing Security Groups
+############################################
 
-# Existing ALB SG
-data "aws_security_group" "alb_sg" {
+data "aws_security_group" "alb" {
   id = var.existing_alb_sg_id
 }
 
-# Existing ECS tasks SG
-data "aws_security_group" "ecs_tasks" {
-  id = var.existing_ecs_tasks_sg_id
-}
-
-# Existing RDS SG
 data "aws_security_group" "rds" {
   id = var.existing_rds_sg_id
+}
+
+data "aws_security_group" "ecs_tasks" {
+  id = var.existing_ecs_tasks_sg_id
 }
