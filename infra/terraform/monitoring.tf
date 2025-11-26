fcca940a43da40c3e114e62fa7608d3beee0043f
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 15:43:22 -0600

    Update CloudWatch alarms for ALB thresholds

--- infra/terraform/monitoring.tf
+++ infra/terraform/monitoring.tf
@@ -1,6 +1,6 @@
-##############################################
-# CloudWatch Alarms for ALB
-##############################################
+#############################################
+# CloudWatch Alarm - ALB 5XX Errors
+#############################################
 
 resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
   alarm_name          = "${var.project_name}-alb-5xx"
@@ -10,12 +10,17 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
   namespace           = "AWS/ApplicationELB"
   period              = 60
   statistic           = "Sum"
-  threshold           = 1
+  threshold           = 5
+  treat_missing_data  = "notBreaching"
 
   dimensions = {
-    LoadBalancer = data.aws_lb.webapp_alb.arn_suffix
+    LoadBalancer = aws_lb.webapp_alb.arn_suffix
   }
 }
+
+#############################################
+# CloudWatch Alarm - ALB Latency
+#############################################
 
 resource "aws_cloudwatch_metric_alarm" "alb_latency" {
   alarm_name          = "${var.project_name}-alb-latency"
@@ -25,9 +30,10 @@ resource "aws_cloudwatch_metric_alarm" "alb_latency" {
   namespace           = "AWS/ApplicationELB"
   period              = 60
   statistic           = "Average"
-  threshold           = 1
+  threshold           = 2
+  treat_missing_data  = "notBreaching"
 
   dimensions = {
-    LoadBalancer = data.aws_lb.webapp_alb.arn_suffix
+    LoadBalancer = aws_lb.webapp_alb.arn_suffix
   }
 }
