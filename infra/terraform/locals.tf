cf3da3d1c58157762cc493a7b577067a89e19d3e
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-25 19:25:45 -0600

    Add locals for subnets in Terraform configuration
    Define locals for public and private subnets used by ECS, RDS, and ALB.

---
+++ infra/terraform/locals.tf
@@ -0,0 +1,8 @@
+##############################################
+# Locals for Subnets (Used by ECS, RDS, ALB)
+##############################################
+
+locals {
+  public_subnets  = aws_subnet.public[*].id
+  private_subnets = aws_subnet.private[*].id
+}
