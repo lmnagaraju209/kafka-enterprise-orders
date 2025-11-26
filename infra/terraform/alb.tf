d1c4da4a6bf4d6406822f10e64fae66c07019ce1
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 14:48:49 -0600

    Refactor ALB configuration and update security groups

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,68 +1,29 @@
-##############################################
-# Security Group for ALB
-##############################################
+############################################
+# ALB (uses existing VPC + SG)
+############################################
 
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.project_name}-alb-sg"
-  description = "ALB security group"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-##############################################
-# Application Load Balancer
-##############################################
-
-resource "aws_lb" "webapp_alb" {
+resource "aws_lb" "web" {
   name               = "${var.project_name}-alb"
-  internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = aws_subnet.public[*].id
-
-  enable_deletion_protection = false
+  security_groups    = [data.aws_security_group.alb.id]
+  subnets            = local.public_subnets
 }
-
-##############################################
-# Target Group for Backend Pods / ECS Tasks
-##############################################
 
 resource "aws_lb_target_group" "webapp_tg" {
-  name        = "${var.project_name}-tg"
-  port        = 80
-  protocol    = "HTTP"
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.main.id
   target_type = "ip"
-  vpc_id      = aws_vpc.main.id
 
   health_check {
-    path                = "/healthz"
-    interval            = 30
-    timeout             = 5
-    unhealthy_threshold = 2
-    healthy_threshold   = 2
+    path = "/"
+    port = "traffic-port"
   }
 }
 
-##############################################
-# ALB Listener
-##############################################
-
-resource "aws_lb_listener" "webapp_listener" {
-  load_balancer_arn = aws_lb.webapp_alb.arn
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web.arn
   port              = 80
   protocol          = "HTTP"
 
@@ -70,12 +31,4 @@ resource "aws_lb_listener" "webapp_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.webapp_tg.arn
   }
-}
-
-##############################################
-# Output ALB DNS
-##############################################
-
-output "alb_dns_name" {
-  value = aws_lb.webapp_alb.dns_name
 }
