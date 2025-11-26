3a7e3a3471fe926aa115581947494de6c75354c4
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:24:35 -0600

    Update locals.tf

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -3,7 +3,10 @@ locals {
   private_subnets = var.existing_private_subnet_ids
   public_subnets  = var.existing_public_subnet_ids
 
-  ecs_tasks_sg = var.existing_ecs_tasks_sg_id
-  rds_sg       = var.existing_rds_sg_id
-  alb_sg       = var.existing_alb_sg_id
+  # SECURITY GROUPS WE ARE REUSING
+  rds_sg          = var.existing_rds_sg_id
+  ecs_tasks_sg    = var.existing_ecs_tasks_sg_id
+
+  # ALB SG IS NEW (created in alb.tf)
+  alb_sg          = aws_security_group.alb_sg.id
 }
