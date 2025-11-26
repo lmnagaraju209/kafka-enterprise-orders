9eaea1bd12ad25c47fe39ec61b96b7d044c7782c
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 14:26:56 -0600

    Switch to existing VPC and subnet data sources
    Updated VPC configuration to use existing VPC and subnets.

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -1,122 +1,21 @@
 ##############################################
-# VPC
+# USE EXISTING VPC AND EXISTING SUBNETS
 ##############################################
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# Terraform will NOT create VPC.
+# Terraform will NOT create SUBNETS.
+# It only reads (data sources) and uses them.
 
-  tags = {
-    Name = "${var.project_name}-vpc"
-  }
+data "aws_vpc" "main" {
+  id = var.existing_vpc_id
 }
 
-##############################################
-# INTERNET GATEWAY
-##############################################
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.project_name}-igw"
-  }
+data "aws_subnet" "private" {
+  for_each = toset(var.existing_private_subnet_ids)
+  id       = each.value
 }
 
-##############################################
-# PUBLIC SUBNETS
-##############################################
-
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet("10.10.0.0/16", 4, count.index)
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.project_name}-public-${count.index}"
-  }
+data "aws_subnet" "public" {
+  for_each = toset(var.existing_public_subnet_ids)
+  id       = each.value
 }
-
-##############################################
-# PRIVATE SUBNETS
-##############################################
-
-resource "aws_subnet" "private" {
-  count     = 2
-  vpc_id    = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.10.0.0/16", 4, count.index + 10)
-
-  tags = {
-    Name = "${var.project_name}-private-${count.index}"
-  }
-}
-
-##############################################
-# NAT GATEWAY + EIP
-##############################################
-
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-
-  tags = {
-    Name = "${var.project_name}-nat-eip"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public[0].id
-
-  tags = {
-    Name = "${var.project_name}-nat"
-  }
-}
-
-##############################################
-# PUBLIC ROUTE TABLE
-##############################################
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-##############################################
-# PRIVATE ROUTE TABLE
-##############################################
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "${var.project_name}-private-rt"
-  }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  count          = 2
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private_rt.id
-}
-
