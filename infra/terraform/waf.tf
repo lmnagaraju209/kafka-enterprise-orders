d08bf5e09273e953a791b43deade03e2fd4d7e51
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:09:58 -0600

    Refactor WAF configuration for existing ALB

--- infra/terraform/waf.tf
+++ infra/terraform/waf.tf
@@ -1,19 +1,14 @@
-###############################
-# EXISTING ALB LOOKUP
-###############################
+###############################################
+# WAF – Attach to existing ALB
+###############################################
 
 data "aws_lb" "webapp_alb" {
   arn = var.existing_alb_arn
 }
 
-###############################
-# CREATE WAF
-###############################
-
 resource "aws_wafv2_web_acl" "webapp" {
-  name        = "${var.project_name}-waf"
-  description = "WAF for webapp"
-  scope       = "REGIONAL"
+  name  = "${var.project_name}-waf"
+  scope = "REGIONAL"
 
   default_action {
     allow {}
@@ -21,38 +16,12 @@ resource "aws_wafv2_web_acl" "webapp" {
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "webapp-waf"
+    metric_name                = "${var.project_name}-waf"
     sampled_requests_enabled   = true
-  }
-
-  rule {
-    name     = "AWS-AWSManagedRulesCommonRuleSet"
-    priority = 0
-
-    override_action {
-      none {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "awsCommonRules"
-      sampled_requests_enabled   = true
-    }
   }
 }
 
-###############################
-# WAF ASSOCIATION TO EXISTING ALB
-###############################
-
 resource "aws_wafv2_web_acl_association" "webapp" {
-  resource_arn = data.aws_lb.webapp_alb.arn
+  resource_arn = var.existing_alb_arn
   web_acl_arn  = aws_wafv2_web_acl.webapp.arn
 }
