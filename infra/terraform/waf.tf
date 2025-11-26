73677d75fdd507341a801c99b1da292c7ea18498
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:05:26 -0600

    Update WAF configuration for existing ALB

--- infra/terraform/waf.tf
+++ infra/terraform/waf.tf
@@ -1,19 +1,33 @@
-#############################################
-# WAF Web ACL (new)
-#############################################
+###############################
+# EXISTING ALB LOOKUP
+###############################
 
-resource "aws_wafv2_web_acl" "main" {
+data "aws_lb" "webapp_alb" {
+  arn = var.existing_alb_arn
+}
+
+###############################
+# CREATE WAF
+###############################
+
+resource "aws_wafv2_web_acl" "webapp" {
   name        = "${var.project_name}-waf"
-  description = "WAF for ALB"
+  description = "WAF for webapp"
   scope       = "REGIONAL"
 
   default_action {
     allow {}
   }
 
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "webapp-waf"
+    sampled_requests_enabled   = true
+  }
+
   rule {
     name     = "AWS-AWSManagedRulesCommonRuleSet"
-    priority = 1
+    priority = 0
 
     override_action {
       none {}
@@ -28,23 +42,17 @@ resource "aws_wafv2_web_acl" "main" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "common-rule-set"
+      metric_name                = "awsCommonRules"
       sampled_requests_enabled   = true
     }
   }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "${var.project_name}-waf"
-    sampled_requests_enabled   = true
-  }
 }
 
-#############################################
-# WAF Association with ALB
-#############################################
+###############################
+# WAF ASSOCIATION TO EXISTING ALB
+###############################
 
 resource "aws_wafv2_web_acl_association" "webapp" {
-  resource_arn = aws_lb.webapp_alb.arn
-  web_acl_arn  = aws_wafv2_web_acl.main.arn
+  resource_arn = data.aws_lb.webapp_alb.arn
+  web_acl_arn  = aws_wafv2_web_acl.webapp.arn
 }
