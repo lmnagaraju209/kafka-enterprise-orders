e96bd48780dd4706c6394d66dd4b42ed34780897
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 15:00:29 -0600

    Refactor existing security groups data blocks

--- infra/terraform/security_groups.tf
+++ infra/terraform/security_groups.tf
@@ -1,15 +1,15 @@
-############################################
-# Reuse Existing Security Groups
-############################################
+##############################################
+# EXISTING SECURITY GROUPS (NO CREATION)
+##############################################
 
-data "aws_security_group" "alb" {
-  id = var.existing_alb_sg_id
+data "aws_security_group" "ecs_tasks" {
+  id = var.existing_ecs_tasks_sg_id
 }
 
 data "aws_security_group" "rds" {
   id = var.existing_rds_sg_id
 }
 
-data "aws_security_group" "ecs_tasks" {
-  id = var.existing_ecs_tasks_sg_id
+data "aws_security_group" "alb" {
+  id = var.existing_alb_sg_id
 }
