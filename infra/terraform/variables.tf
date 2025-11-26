f64ae290b51d7e393a3e45a7a65763193938ab98
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:20:54 -0600

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,57 +1,20 @@
-#####################################################
-# Shared Variables
-#####################################################
-
-variable "project_name" {
-  type        = string
-  default     = "kafka-enterprise-orders"
-}
+########################################
+# General
+########################################
 
 variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
 
-variable "existing_vpc_id" {
+variable "project_name" {
   type        = string
+  default     = "kafka-enterprise-orders"
 }
 
-variable "existing_public_subnet_ids" {
-  type        = list(string)
-}
-
-variable "existing_private_subnet_ids" {
-  type        = list(string)
-}
-
-variable "existing_ecs_tasks_sg_id" {
-  type        = string
-}
-
-variable "existing_rds_sg_id" {
-  type        = string
-}
-
-variable "existing_alb_arn" {
-  type        = string
-}
-
-variable "existing_alb_sg_id" {
-  type        = string
-}
-
-variable "existing_alb_listener_arn" {
-  type        = string
-}
-
-variable "rds_password" {
-  type      = string
-  sensitive = true
-}
-
-#####################################################
-# IMAGES
-#####################################################
+########################################
+# Containers (from CI/CD)
+########################################
 
 variable "container_image_producer" {
   type = string
@@ -77,9 +40,9 @@ variable "web_frontend_image" {
   type = string
 }
 
-#####################################################
-# CONFLUENT
-#####################################################
+########################################
+# Confluent Cloud
+########################################
 
 variable "confluent_bootstrap_servers" {
   type = string
@@ -94,3 +57,92 @@ variable "confluent_api_secret" {
   type      = string
   sensitive = true
 }
+
+########################################
+# Kafka Topics
+########################################
+
+variable "orders_topic" {
+  type    = string
+  default = "orders"
+}
+
+variable "fraud_alerts_topic" {
+  type    = string
+  default = "fraud-alerts"
+}
+
+variable "payments_topic" {
+  type    = string
+  default = "payments"
+}
+
+variable "order_analytics_topic" {
+  type    = string
+  default = "order-analytics"
+}
+
+########################################
+# Couchbase
+########################################
+
+variable "couchbase_host" {
+  type    = string
+  default = "couchbase"
+}
+
+variable "couchbase_bucket" {
+  type    = string
+  default = "order_analytics"
+}
+
+variable "couchbase_username" {
+  type    = string
+  default = "Administrator"
+}
+
+variable "couchbase_password" {
+  type      = string
+  default   = "password"
+  sensitive = true
+}
+
+########################################
+# RDS Credentials
+########################################
+
+variable "rds_username" {
+  type    = string
+  default = "orders_user"
+}
+
+variable "rds_password" {
+  type      = string
+  sensitive = true
+}
+
+########################################
+# EXISTING VPC + SGs
+########################################
+
+variable "existing_vpc_id" {
+  type = string
+}
+
+variable "existing_private_subnet_ids" {
+  type = list(string)
+}
+
+variable "existing_public_subnet_ids" {
+  type = list(string)
+}
+
+variable "existing_rds_sg_id" {
+  type = string
+}
+
+variable "existing_ecs_tasks_sg_id" {
+  type = string
+}
+
+# ALB SG will be CREATED — NO VARIABLE NEEDED
