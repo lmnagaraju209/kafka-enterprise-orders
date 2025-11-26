c6558966f45490b4da329f49f566e1c5e6fb9e6c
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 14:56:14 -0600

    Update alb.tf

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,29 +1,37 @@
-############################################
-# ALB (uses existing VPC + SG)
-############################################
+##############################################
+# USE EXISTING ALB (DATA SOURCE)
+##############################################
 
-resource "aws_lb" "web" {
-  name               = "${var.project_name}-alb"
-  load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.alb.id]
-  subnets            = local.public_subnets
+data "aws_lb" "webapp_alb" {
+  name = "${var.project_name}-alb"
 }
 
+##############################################
+# TARGET GROUP (still created by Terraform)
+##############################################
+
 resource "aws_lb_target_group" "webapp_tg" {
-  name     = "${var.project_name}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.main.id
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
   target_type = "ip"
+  vpc_id      = var.existing_vpc_id
 
   health_check {
-    path = "/"
-    port = "traffic-port"
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.web.arn
+##############################################
+# LISTENER (uses existing ALB)
+##############################################
+
+resource "aws_lb_listener" "webapp_listener" {
+  load_balancer_arn = data.aws_lb.webapp_alb.arn
   port              = 80
   protocol          = "HTTP"
 
