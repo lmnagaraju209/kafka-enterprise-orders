a689f4cb4bf8cad2fd43e4e6663f7eba7d918542
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 14:28:24 -0600

    Remove comment on existing subnets
    Removed comment about reusing existing subnets.

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -3,7 +3,6 @@
 ##############################################
 
 locals {
-  # Reuse existing subnets from variables
   public_subnets  = var.existing_public_subnet_ids
   private_subnets = var.existing_private_subnet_ids
 }
