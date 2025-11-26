f63b5053f291572139f3876662ee1595cf129b5c
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:43:36 -0600

    Refactor CloudWatch alarms for ALB monitoring
    Updated CloudWatch alarms for ALB to use new thresholds and resource references.

--- infra/terraform/monitoring.tf
+++ infra/terraform/monitoring.tf
@@ -1,33 +1,29 @@
-########################################
-# CloudWatch Alarms for ALB
-########################################
-
 resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
-  alarm_name          = "${var.project_name}-alb-5xx"
+  alarm_name          = "alb-5xx-errors"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HTTPCode_ELB_5XX_Count"
-  namespace           = "AWS/ApplicationELB"
+  namespace           = "AWS/ELB"
   period              = 60
   statistic           = "Sum"
-  threshold           = 5
+  threshold           = 1
 
   dimensions = {
-    LoadBalancer = aws_lb.webapp_alb.arn_suffix
+    LoadBalancer = data.aws_lb.webapp_alb.arn_suffix
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alb_latency" {
-  alarm_name          = "${var.project_name}-alb-latency"
+  alarm_name          = "alb-latency"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "Latency"
-  namespace           = "AWS/ApplicationELB"
+  namespace           = "AWS/ELB"
   period              = 60
   statistic           = "Average"
-  threshold           = 1
+  threshold           = 0.5
 
   dimensions = {
-    LoadBalancer = aws_lb.webapp_alb.arn_suffix
+    LoadBalancer = data.aws_lb.webapp_alb.arn_suffix
   }
 }
