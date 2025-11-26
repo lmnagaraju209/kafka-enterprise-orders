106f349d1d3b8b7eea058cb6fd4d1ca8e44c0e5c
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-26 16:21:17 -0600

    Refactor ALB configuration with security group and listener

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,15 +1,69 @@
-###############################################
-# IMPORT EXISTING ALB ONLY (NO VARIABLES HERE)
-###############################################
+########################################
+# ALB SECURITY GROUP
+########################################
 
-data "aws_lb" "webapp_alb" {
-  arn = var.existing_alb_arn
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "ALB security group"
+  vpc_id      = var.existing_vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-data "aws_security_group" "alb" {
-  id = var.existing_alb_sg_id
+########################################
+# APPLICATION LOAD BALANCER
+########################################
+
+resource "aws_lb" "webapp_alb" {
+  name               = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.existing_public_subnet_ids
 }
 
-data "aws_lb_listener" "webapp_listener" {
-  arn = var.existing_alb_listener_arn
+########################################
+# TARGET GROUP
+########################################
+
+resource "aws_lb_target_group" "webapp_tg" {
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.existing_vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 15
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+########################################
+# LISTENER
+########################################
+
+resource "aws_lb_listener" "webapp_listener" {
+  load_balancer_arn = aws_lb.webapp_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webapp_tg.arn
+  }
 }
