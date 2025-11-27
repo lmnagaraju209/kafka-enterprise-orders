bd54cce0b805178d521b9effbaf84d714cb35122
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-27 12:09:06 -0600

    Refactor ALB configuration and target groups

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,51 +1,47 @@
 ###############################################
-# EXISTING TARGET GROUPS (already in AWS)
+# APPLICATION LOAD BALANCER (EXISTING)
 ###############################################
 
-data "aws_lb_target_group" "backend_tg" {
-  name = "backend-tg"
+data "aws_lb" "webapp_alb" {
+  arn = local.alb_arn
 }
 
-data "aws_lb_target_group" "frontend_tg" {
-  name = "frontend-tg"
-}
-
-data "aws_lb_target_group" "payment_tg" {
-  name = "payment-tg"
-}
-
-data "aws_lb_target_group" "analytics_tg" {
-  name = "analytics-tg"
+data "aws_lb_listener" "webapp_listener" {
+  arn = local.alb_listener_arn
 }
 
 ###############################################
-# APPLICATION LOAD BALANCER
+# TARGET GROUPS
 ###############################################
 
-resource "aws_lb" "ecs_alb" {
-  name               = "${var.project_name}-alb"
-  load_balancer_type = "application"
-  internal           = false
-
-  security_groups = [local.alb_sg_id]
-  subnets         = local.public_subnets
-
-  tags = {
-    Name = "${var.project_name}-alb"
-  }
+resource "aws_lb_target_group" "backend_tg" {
+  name        = "backend-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = var.existing_vpc_id
+  target_type = "ip"
 }
 
-###############################################
-# LISTENER (forward to existing frontend TG by default)
-###############################################
+resource "aws_lb_target_group" "frontend_tg" {
+  name        = "frontend-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.existing_vpc_id
+  target_type = "ip"
+}
 
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+resource "aws_lb_target_group" "payment_tg" {
+  name        = "payment-tg"
+  port        = 8081
+  protocol    = "HTTP"
+  vpc_id      = var.existing_vpc_id
+  target_type = "ip"
+}
 
-  default_action {
-    type             = "forward"
-    target_group_arn = data.aws_lb_target_group.frontend_tg.arn
-  }
+resource "aws_lb_target_group" "analytics_tg" {
+  name        = "analytics-tg"
+  port        = 8082
+  protocol    = "HTTP"
+  vpc_id      = var.existing_vpc_id
+  target_type = "ip"
 }
