8db98e30275af056f4688641e14145d9d4ae4924
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-27 12:17:45 -0600

    Update locals.tf

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -3,13 +3,10 @@
 ###############################################
 
 locals {
-  private_subnets   = var.existing_private_subnet_ids
-  public_subnets    = var.existing_public_subnet_ids
+  private_subnets = var.existing_private_subnet_ids
+  public_subnets  = var.existing_public_subnet_ids
 
-  ecs_tasks_sg      = var.existing_ecs_tasks_sg_id
-  alb_sg            = var.existing_alb_sg_id
-  rds_sg            = var.existing_rds_sg_id
-
-  alb_arn           = var.existing_alb_arn
-  alb_listener_arn  = var.existing_alb_listener_arn
+  ecs_tasks_sg = var.existing_ecs_tasks_sg_id
+  alb_sg       = var.existing_alb_sg_id
+  rds_sg       = var.existing_rds_sg_id
 }
