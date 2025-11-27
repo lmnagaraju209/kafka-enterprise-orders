a0709d6f10a8edc5a527442dc004bcc4a3caf436
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-27 12:32:08 -0600

    Update alb.tf

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,9 +1,24 @@
 ###############################################
+# APPLICATION LOAD BALANCER
+###############################################
+
+resource "aws_lb" "ecs_alb" {
+  name               = "${var.project_name}-alb-main"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups = [local.alb_sg]
+  subnets         = local.public_subnets
+}
+
+###############################################
 # TARGET GROUP – FRONTEND ONLY
 ###############################################
 
 resource "aws_lb_target_group" "frontend_tg" {
-  name        = "fe-tg"
+  # use a prefix so AWS generates a unique name and avoids conflicts
+  name_prefix = "fe-"
+
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
@@ -17,5 +32,20 @@ resource "aws_lb_target_group" "frontend_tg" {
     unhealthy_threshold = 2
     healthy_threshold   = 2
     timeout             = 5
+  }
+}
+
+###############################################
+# LISTENER – HTTP 80
+###############################################
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
   }
 }
