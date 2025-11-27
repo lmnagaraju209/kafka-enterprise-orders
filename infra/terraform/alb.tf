64611e75a6f32d9094d432276bd7e4ad581edc3b
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-11-27 11:10:28 -0600

    Update alb.tf

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,4 +1,24 @@
 ###############################################
+# EXISTING TARGET GROUPS (already in AWS)
+###############################################
+
+data "aws_lb_target_group" "backend_tg" {
+  name = "backend-tg"
+}
+
+data "aws_lb_target_group" "frontend_tg" {
+  name = "frontend-tg"
+}
+
+data "aws_lb_target_group" "payment_tg" {
+  name = "payment-tg"
+}
+
+data "aws_lb_target_group" "analytics_tg" {
+  name = "analytics-tg"
+}
+
+###############################################
 # APPLICATION LOAD BALANCER
 ###############################################
 
@@ -7,7 +27,7 @@ resource "aws_lb" "ecs_alb" {
   load_balancer_type = "application"
   internal           = false
 
-  security_groups = [var.existing_alb_sg_id]
+  security_groups = [local.alb_sg_id]
   subnets         = local.public_subnets
 
   tags = {
@@ -16,43 +36,7 @@ resource "aws_lb" "ecs_alb" {
 }
 
 ###############################################
-# TARGET GROUPS
-###############################################
-
-resource "aws_lb_target_group" "backend_tg" {
-  name        = "backend-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id
-  target_type = "ip"
-}
-
-resource "aws_lb_target_group" "frontend_tg" {
-  name        = "frontend-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id
-  target_type = "ip"
-}
-
-resource "aws_lb_target_group" "payment_tg" {
-  name        = "payment-tg"
-  port        = 8082
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id
-  target_type = "ip"
-}
-
-resource "aws_lb_target_group" "analytics_tg" {
-  name        = "analytics-tg"
-  port        = 8083
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id
-  target_type = "ip"
-}
-
-###############################################
-# LISTENERS
+# LISTENER (forward to existing frontend TG by default)
 ###############################################
 
 resource "aws_lb_listener" "http_listener" {
@@ -62,6 +46,6 @@ resource "aws_lb_listener" "http_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_tg.arn
+    target_group_arn = data.aws_lb_target_group.frontend_tg.arn
   }
 }
