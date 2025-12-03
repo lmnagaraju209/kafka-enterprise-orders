13bca8371ba44beee94a7a0930aeb60648c3e68a
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-02 19:34:19 -0600

    Add ALB listener

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,54 +1,49 @@
 ###############################################
-# APPLICATION LOAD BALANCER
+# APPLICATION LOAD BALANCER (ALB)
 ###############################################
 
 resource "aws_lb" "ecs_alb" {
-  name               = "${local.project_name}-alb-main"
+  name               = "${var.project_name}-alb-main"
   internal           = false
   load_balancer_type = "application"
-
-  security_groups = [
-    var.existing_alb_sg_id
-  ]
-
-  subnets = var.existing_public_subnet_ids
+  security_groups    = [var.existing_alb_sg_id]
+  subnets            = var.existing_public_subnet_ids
 
   tags = {
-    Project = local.project_name
+    Project = var.project_name
   }
 }
 
 ###############################################
-# TARGET GROUP
+# ALB TARGET GROUP
 ###############################################
 
 resource "aws_lb_target_group" "frontend_tg" {
-  name        = "fe-${local.project_name}"
+  name        = "fe-${var.project_name}"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.existing_vpc_id
 
   health_check {
-    enabled             = true
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    interval            = 30
-    timeout             = 5
     path                = "/"
     protocol            = "HTTP"
+    interval            = 30
+    unhealthy_threshold = 3
+    healthy_threshold   = 3
+    timeout             = 5
   }
 
   tags = {
-    Project = local.project_name
+    Project = var.project_name
   }
 }
 
 ###############################################
-# LISTENER
+# ALB LISTENER (THIS WAS MISSING)
 ###############################################
 
-resource "aws_lb_listener" "frontend_listener" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ecs_alb.arn
   port              = 80
   protocol          = "HTTP"
