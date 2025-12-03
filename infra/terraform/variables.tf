5d8fc60e8ac2fdc1479aaf67b8eb2ed8ff8bbd46
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-02 19:31:32 -0600

    Fix variables.tf fully

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,73 +1,114 @@
-##############################
-# VARIABLES FOR TERRAFORM
-##############################
+###############################################
+# PROJECT METADATA
+###############################################
 
-variable "aws_region" {
+variable "project_name" {
+  description = "Project name used for naming ECS, ALB, TG, and other resources"
   type        = string
-  description = "AWS region"
+  default     = "kafka-enterprise-orders"
 }
 
-variable "container_image_producer" {
+variable "aws_region" {
+  description = "AWS region for all resources"
   type        = string
-  description = "Docker image for producer"
+  default     = "us-east-2"
+}
+
+###############################################
+# CONTAINER IMAGES
+###############################################
+
+variable "container_image_producer" {
+  description = "Docker image for producer service"
+  type        = string
 }
 
 variable "container_image_fraud" {
-  type        = string
   description = "Docker image for fraud service"
+  type        = string
 }
 
 variable "container_image_payment" {
-  type        = string
   description = "Docker image for payment service"
+  type        = string
 }
 
 variable "container_image_analytics" {
-  type        = string
   description = "Docker image for analytics service"
+  type        = string
 }
 
+variable "container_image_backend" {
+  description = "Docker image for backend service"
+  type        = string
+}
+
+variable "container_image_frontend" {
+  description = "Docker image for frontend service"
+  type        = string
+}
+
+###############################################
+# CONFLUENT CONNECTION VALUES
+###############################################
+
 variable "confluent_bootstrap_servers" {
+  description = "Confluent Cloud bootstrap servers"
   type        = string
 }
 
 variable "confluent_api_key" {
+  description = "Confluent API Key"
   type        = string
 }
 
 variable "confluent_api_secret" {
+  description = "Confluent API Secret"
   type        = string
+  sensitive   = true
 }
+
+###############################################
+# POSTGRES / RDS
+###############################################
 
 variable "rds_password" {
+  description = "Password for RDS PostgreSQL"
   type        = string
+  sensitive   = true
 }
 
-##############################
-# NETWORK (STATIC INPUTS)
-##############################
+###############################################
+# EXISTING NETWORK (STATIC)
+###############################################
 
 variable "existing_vpc_id" {
-  type = string
+  description = "Existing VPC ID"
+  type        = string
 }
 
 variable "existing_public_subnet_ids" {
-  type = list(string)
+  description = "List of existing PUBLIC subnets"
+  type        = list(string)
 }
 
 variable "existing_private_subnet_ids" {
-  type = list(string)
+  description = "List of existing PRIVATE subnets"
+  type        = list(string)
 }
 
 variable "existing_alb_sg_id" {
-  type = string
+  description = "Existing ALB Security Group"
+  type        = string
 }
 
 variable "existing_ecs_tasks_sg_id" {
-  type = string
+  description = "Existing ECS Tasks Security Group"
+  type        = string
 }
 
 variable "existing_rds_sg_id" {
-  type = string
+  description = "Existing RDS Security Group"
+  type        = string
 }
 
