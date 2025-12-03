a3f59ceb3cfe4791d07200aa8d155127d0d95909
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-02 21:28:10 -0600

    fix monitoring locals

--- infra/terraform/monitoring.tf
+++ infra/terraform/monitoring.tf
@@ -1,9 +1,9 @@
-###############################################
-# ALB CLOUDWATCH ALARMS
-###############################################
+###########################################
+# ALB 5XX ERROR ALARM
+###########################################
 
 resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
-  alarm_name          = "${local.project_name}-alb-5xx-errors"
+  alarm_name          = "${var.project_name}-alb-5xx-errors"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HTTPCode_ELB_5XX_Count"
@@ -11,15 +11,21 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
   period              = 60
   statistic           = "Sum"
   threshold           = 1
-  treat_missing_data  = "missing"
 
   dimensions = {
     LoadBalancer = aws_lb.ecs_alb.arn_suffix
   }
+
+  alarm_description = "High number of 5XX errors on ALB"
+  actions_enabled   = false
 }
 
+###########################################
+# ALB LATENCY ALARM
+###########################################
+
 resource "aws_cloudwatch_metric_alarm" "alb_latency" {
-  alarm_name          = "${local.project_name}-alb-latency"
+  alarm_name          = "${var.project_name}-alb-latency"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "TargetResponseTime"
@@ -27,9 +33,12 @@ resource "aws_cloudwatch_metric_alarm" "alb_latency" {
   period              = 60
   statistic           = "Average"
   threshold           = 1
-  treat_missing_data  = "missing"
 
   dimensions = {
     LoadBalancer = aws_lb.ecs_alb.arn_suffix
   }
+
+  alarm_description = "High latency detected in ALB"
+  actions_enabled   = false
 }
+
