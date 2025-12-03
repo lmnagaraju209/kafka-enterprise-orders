05d8d2383de511b7b1c46795e7d300116b686a2f
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-02 21:34:29 -0600

    fix target group for fargate ip mode

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -1,28 +1,42 @@
+###########################################
+# APPLICATION LOAD BALANCER
+###########################################
+
 resource "aws_lb" "ecs_alb" {
   name               = "${var.project_name}-alb-main"
   internal           = false
   load_balancer_type = "application"
-  subnets            = local.public_subnets
   security_groups    = [local.alb_sg]
+  subnets            = local.public_subnets
 }
 
+###########################################
+# TARGET GROUP (MUST BE ip)
+###########################################
+
 resource "aws_lb_target_group" "frontend_tg" {
-  name     = "fe-${var.project_name}"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  name        = "fe-${var.project_name}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = local.vpc_id
+  target_type = "ip"   # <<< THIS FIXES YOUR ERROR
 
   health_check {
-    enabled  = true
-    protocol = "HTTP"
-    path     = "/"
-    port     = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200-399"
   }
 }
 
+###########################################
+# LISTENER
+###########################################
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = 80
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
