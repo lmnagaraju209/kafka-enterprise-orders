034528bfcce2cac64c77d47fcb1c52aadee2ff8c
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-03 18:33:33 -0600

    Fix ECS outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,20 +1,10 @@
 output "ecs_cluster_name" {
   description = "ECS cluster name"
-  value       = aws_ecs_cluster.main.name
+  value       = aws_ecs_cluster.this.name
 }
 
 output "ecs_cluster_arn" {
   description = "ECS cluster ARN"
-  value       = aws_ecs_cluster.main.arn
-}
-
-output "alb_dns_name" {
-  description = "Public DNS name of the ALB"
-  value       = aws_lb.ecs_alb.dns_name
-}
-
-output "alb_arn" {
-  description = "ARN of the ALB"
-  value       = aws_lb.ecs_alb.arn
+  value       = aws_ecs_cluster.this.arn
 }
 
