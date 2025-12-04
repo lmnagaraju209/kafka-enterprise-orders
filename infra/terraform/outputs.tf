26ef9fb52719b84bfc3fb23dea1aef36cce271d4
Aisalkyn <aisalkyn@Aisalkyns-MacBook-Pro.local>
2025-12-03 21:45:42 -0600

    l

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -23,4 +23,22 @@ output "payment_service_name" {
 output "analytics_service_name" {
   value = aws_ecs_service.analytics.name
 }
+###############################################
+# EKS OUTPUTS
+###############################################
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "eks_cluster_ca" {
+  description = "EKS cluster certificate authority data"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
 
